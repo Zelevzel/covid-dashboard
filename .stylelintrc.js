9308abbf185eb003e20f26da26f4345803d04667
Evgeniy <zelevzel@ya.ru>
2020-12-21 16:36:32 +0300

    feat: added stylelint plugin and config

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  extends: ["stylelint-config-airbnb",
+            "stylelint-config-rational-order",
+            "stylelint-prettier/recommended"],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+    // Дополнительные правила
+  }
+};
